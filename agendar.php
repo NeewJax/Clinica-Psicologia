1c6d2b10ea6b84c222f52788e1413116f831ef21
Victor-br-santo <victorbrsanto@Gmail.com>
2023-10-23 18:51:36 -0300

    modified:   agendar.php

--- agendar.php
+++ agendar.php
@@ -57,7 +57,7 @@ if (isset($_POST['enviar'])) {
 <body>
  <!-- Navbar Start -->
  <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 wow fadeIn" data-wow-delay="0.1s">
-        <img id="estacio" src="logo-estacio.png" alt="Estácio de Sá">
+        <img id="estacio" src="img/logo-estacio.png" alt="Estácio de Sá">
     </a>
 </nav>
     
