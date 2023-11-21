1f7ab7e146f7c87b7f41723e1b9dfe681c357595
Victor-br-santo <victorbrsanto@Gmail.com>
2023-11-21 12:26:23 -0300

    modified: ajuste tela_escolha_logins

--- terapeuta/script.js
+++ terapeuta/script.js
@@ -1,12 +1,14 @@
-// Seleciona o botão de login
-const btnLogin = document.querySelector('.btn-login');
+// // Seleciona o botão de login
+// const btnLogin = document.querySelector('.btn-login');
 
-// Adiciona um ouvinte de evento para quando o cursor entra no botão
-btnLogin.addEventListener('mouseenter', function() {
-    btnLogin.classList.add('btn-login-hover');
-});
+// // Adiciona um ouvinte de evento para quando o cursor entra no botão
+// btnLogin.addEventListener('mouseenter', function() {
+//     btnLogin.classList.add('btn-login-hover');
+// });
 
-// Adiciona um ouvinte de evento para quando o cursor sai do botão
-btnLogin.addEventListener('mouseleave', function() {
-    btnLogin.classList.remove('btn-login-hover');
-});
+// // Adiciona um ouvinte de evento para quando o cursor sai do botão
+// btnLogin.addEventListener('mouseleave', function() {
+//     btnLogin.classList.remove('btn-login-hover');
+// });
+
+
