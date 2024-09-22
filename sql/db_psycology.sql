ab529fa78e531ed6371b96c3c6333650fb17036c
Wilson-Pedro <wilsonpedroneto@gmail.com>
2024-09-22 09:18:31 -0300

    feat: tbl_horario_sala, tbl_turno, tbl_semana, tbl_status_sala, tbl_sala_reservada

--- sql/db_psycology.sql
+++ sql/db_psycology.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 14/09/2024 às 15:59
+-- Tempo de geração: 22/09/2024 às 14:16
 -- Versão do servidor: 10.6.15-MariaDB
 -- Versão do PHP: 8.2.0
 
@@ -216,6 +216,31 @@ INSERT INTO `tbl_genero` (`id`, `sexo`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `tbl_horario_sala`
+--
+
+CREATE TABLE `tbl_horario_sala` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `horario` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tbl_horario_sala`
+--
+
+INSERT INTO `tbl_horario_sala` (`id`, `horario`) VALUES
+(1, '08H'),
+(2, '09H'),
+(3, '10H'),
+(4, '11H'),
+(5, '14H'),
+(6, '15H'),
+(7, '16H'),
+(8, '17H');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `tbl_logradouro`
 --
 
@@ -320,6 +345,147 @@ INSERT INTO `tbl_renda_familiar` (`id`, `renda`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `tbl_sala_reservada`
+--
+
+CREATE TABLE `tbl_sala_reservada` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_turno` int(11) UNSIGNED NOT NULL,
+  `id_horario` int(11) UNSIGNED NOT NULL,
+  `id_semana` int(11) UNSIGNED NOT NULL,
+  `id_status` int(11) UNSIGNED NOT NULL,
+  `sala_cod` varchar(10) NOT NULL,
+  `sala` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tbl_sala_reservada`
+--
+
+INSERT INTO `tbl_sala_reservada` (`id`, `id_turno`, `id_horario`, `id_semana`, `id_status`, `sala_cod`, `sala`) VALUES
+(1, 1, 1, 1, 5, 'm_8_s1', 'Atend. Infantil'),
+(2, 1, 1, 1, 5, 'm_8_s2', 'Atend. Grupo'),
+(3, 1, 1, 1, 5, 'm_8_s3', 'Atend. I'),
+(4, 1, 1, 1, 5, 'm_8_s4', 'Eline de Sousa(II)'),
+(5, 1, 1, 1, 5, 'm_8_s5', 'Atend. III'),
+(6, 1, 1, 1, 5, 'm_8_s6', 'Atend. IV'),
+(7, 1, 1, 1, 5, 'm_8_s7', 'Atend. Infantil'),
+(8, 1, 2, 1, 5, 'm_9_s1', 'Henrique Gabriel(Infantil)'),
+(9, 1, 2, 1, 5, 'm_9_s2', 'Atend. Grupo'),
+(10, 1, 2, 1, 5, 'm_9_s3', '(I)'),
+(11, 1, 2, 1, 5, 'm_9_s4', 'Jessyea Karina(II)'),
+(12, 1, 2, 1, 5, 'm_9_s5', 'Auana Maria'),
+(13, 1, 2, 1, 5, 'm_9_s6', 'Atend. IV'),
+(14, 1, 2, 1, 5, 'm_9_s7', 'Atend. V'),
+(15, 1, 3, 1, 5, 'm_10_s1', 'Atend. Infantil'),
+(16, 1, 3, 1, 5, 'm_10_s2', 'Atend. Grupo'),
+(17, 1, 3, 1, 5, 'm_10_s3', '(I)'),
+(18, 1, 3, 1, 5, 'm_10_s4', 'Maryana Dállia(II)'),
+(19, 1, 3, 1, 5, 'm_10_s5', 'Yuri(III)'),
+(20, 1, 3, 1, 5, 'm_10_s6', 'Ranilson(IV)'),
+(21, 1, 3, 1, 5, 'm_10_s7', 'Tean Rayzton(V)'),
+(22, 1, 4, 1, 5, 'm_11_s1', 'Luiz Márcio(Infantil)'),
+(23, 1, 4, 1, 5, 'm_11_s2', 'Atend. Grupo'),
+(24, 1, 4, 1, 5, 'm_11_s3', '(I)'),
+(25, 1, 4, 1, 5, 'm_11_s4', 'GiulliaCarolina'),
+(26, 1, 4, 1, 5, 'm_11_s5', 'Ildenize(III)'),
+(27, 1, 4, 1, 5, 'm_11_s6', '(IV)'),
+(28, 1, 4, 1, 5, 'm_11_s7', 'Jenice(V)'),
+(29, 2, 5, 1, 5, 't_14_s1', 'Atend. Infantil'),
+(30, 2, 5, 1, 5, 't_14_s2', 'Atend. Grupo'),
+(31, 2, 5, 1, 5, 't_14_s3', 'Atend. I'),
+(32, 2, 5, 1, 5, 't_14_s4', 'Eline de Sousa(II)'),
+(33, 2, 5, 1, 5, 't_14_s5', 'Atend. III'),
+(34, 2, 5, 1, 5, 't_14_s6', 'Atend. IV'),
+(35, 2, 5, 1, 5, 't_14_s7', 'Atend. V'),
+(36, 2, 6, 1, 5, 't_15_s1', 'Henrique Gabriel(Infantil)'),
+(37, 2, 6, 1, 5, 't_15_s2', 'Atend. Grupo'),
+(38, 2, 6, 1, 5, 't_15_s3', '(I)'),
+(39, 2, 6, 1, 5, 't_15_s4', 'Jessyea Karina(II)'),
+(40, 2, 6, 1, 5, 't_15_s5', 'Auana Maria'),
+(41, 2, 6, 1, 5, 't_15_s6', 'Atend. IV'),
+(42, 2, 6, 1, 5, 't_15_s7', 'Atend. V'),
+(43, 2, 7, 1, 5, 't_16_s1', 'Atend. Infantil'),
+(44, 2, 7, 1, 5, 't_16_s2', 'Atend. Grupo'),
+(45, 2, 7, 1, 5, 't_16_s3', '(I)'),
+(46, 2, 7, 1, 5, 't_16_s4', 'Maryana Dállia(II)'),
+(47, 2, 7, 1, 5, 't_16_s5', 'Yuri(III)'),
+(48, 2, 7, 1, 5, 't_16_s6', 'Ranilson(IV)'),
+(49, 2, 7, 1, 5, 't_16_s7', 'Tean Rayzton(V)'),
+(50, 2, 8, 1, 5, 't_17_s1', 'Luiz Márcio(Infantil)'),
+(51, 2, 8, 1, 5, 't_17_s2', 'Atend. Grupo'),
+(52, 2, 8, 1, 5, 't_17_s3', '(I)'),
+(53, 2, 8, 1, 5, 't_17_s4', 'Giullia Carolina'),
+(54, 2, 8, 1, 5, 't_17_s5', 'Ildenize(III)'),
+(55, 2, 8, 1, 5, 't_17_s6', '(IV)'),
+(56, 2, 8, 1, 5, 't_17_s7', 'Jenice(V)');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tbl_semana`
+--
+
+CREATE TABLE `tbl_semana` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `semana` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tbl_semana`
+--
+
+INSERT INTO `tbl_semana` (`id`, `semana`) VALUES
+(1, 'segunda'),
+(2, 'terça'),
+(3, 'quarta'),
+(4, 'quinta'),
+(5, 'sexta');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tbl_status_sala`
+--
+
+CREATE TABLE `tbl_status_sala` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tbl_status_sala`
+--
+
+INSERT INTO `tbl_status_sala` (`id`, `status`) VALUES
+(1, 'reservada'),
+(2, 'livre'),
+(3, 'encaixe'),
+(4, 'triagem'),
+(5, 'nada');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tbl_turno`
+--
+
+CREATE TABLE `tbl_turno` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `turno` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tbl_turno`
+--
+
+INSERT INTO `tbl_turno` (`id`, `turno`) VALUES
+(1, 'MANHÃ'),
+(2, 'TARDE');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `tbl_users`
 --
 
@@ -413,6 +579,12 @@ ALTER TABLE `tbl_genero`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices de tabela `tbl_horario_sala`
+--
+ALTER TABLE `tbl_horario_sala`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices de tabela `tbl_logradouro`
 --
 ALTER TABLE `tbl_logradouro`
@@ -441,6 +613,34 @@ ALTER TABLE `tbl_profissao`
 -- Índices de tabela `tbl_renda_familiar`
 --
 ALTER TABLE `tbl_renda_familiar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `tbl_sala_reservada`
+--
+ALTER TABLE `tbl_sala_reservada`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_sala_reservada_turno` (`id_turno`),
+  ADD KEY `fk_sala_reservada_horario` (`id_horario`),
+  ADD KEY `fk_sala_reservada_status` (`id_status`),
+  ADD KEY `fk_sala_reservada_semana` (`id_semana`);
+
+--
+-- Índices de tabela `tbl_semana`
+--
+ALTER TABLE `tbl_semana`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `tbl_status_sala`
+--
+ALTER TABLE `tbl_status_sala`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `tbl_turno`
+--
+ALTER TABLE `tbl_turno`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -490,6 +690,12 @@ ALTER TABLE `tbl_genero`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT de tabela `tbl_horario_sala`
+--
+ALTER TABLE `tbl_horario_sala`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT de tabela `tbl_logradouro`
 --
 ALTER TABLE `tbl_logradouro`
@@ -514,6 +720,30 @@ ALTER TABLE `tbl_renda_familiar`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT de tabela `tbl_sala_reservada`
+--
+ALTER TABLE `tbl_sala_reservada`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
+--
+-- AUTO_INCREMENT de tabela `tbl_semana`
+--
+ALTER TABLE `tbl_semana`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `tbl_status_sala`
+--
+ALTER TABLE `tbl_status_sala`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `tbl_turno`
+--
+ALTER TABLE `tbl_turno`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- Restrições para tabelas despejadas
 --
 
@@ -535,6 +765,15 @@ ALTER TABLE `tbl_paciente`
   ADD CONSTRAINT `fk_paciente_genero` FOREIGN KEY (`id_genero`) REFERENCES `tbl_genero` (`id`),
   ADD CONSTRAINT `fk_paciente_profissao` FOREIGN KEY (`id_profissao`) REFERENCES `tbl_profissao` (`id`),
   ADD CONSTRAINT `fk_paciente_renda_familar` FOREIGN KEY (`id_renda_familiar`) REFERENCES `tbl_renda_familiar` (`id`);
+
+--
+-- Restrições para tabelas `tbl_sala_reservada`
+--
+ALTER TABLE `tbl_sala_reservada`
+  ADD CONSTRAINT `fk_sala_reservada_horario` FOREIGN KEY (`id_horario`) REFERENCES `tbl_horario_sala` (`id`),
+  ADD CONSTRAINT `fk_sala_reservada_semana` FOREIGN KEY (`id_semana`) REFERENCES `tbl_semana` (`id`),
+  ADD CONSTRAINT `fk_sala_reservada_status` FOREIGN KEY (`id_status`) REFERENCES `tbl_status_sala` (`id`),
+  ADD CONSTRAINT `fk_sala_reservada_turno` FOREIGN KEY (`id_turno`) REFERENCES `tbl_turno` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
